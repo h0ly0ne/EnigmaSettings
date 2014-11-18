c0f0f33a310281c5d98a6c86a159cce57a27a5b0
shaxxx <isakota@gmail.com>
2014-11-18 15:20:08 +0100

    Tiny loging cleanup and NullLogger implementation

---
+++ EnigmaSettings/NullLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Krkadoni.EnigmaSettings.Interfaces;
+
+namespace Krkadoni.EnigmaSettings
+{
+   public class NullLogger : ILog
+    {
+       public void Debug(string message)
+       {
+           
+       }
+
+       public void Warn(string message)
+       {
+           
+       }
+
+       public void Warn(string message, Exception ex)
+       {
+          
+       }
+
+       public void Info(string message)
+       {
+          
+       }
+
+       public void Error(string message)
+       {
+           
+       }
+
+       public void Error(string message, Exception ex)
+       {
+          
+       }
+    }
+}
